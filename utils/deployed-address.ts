1425c2649ee2438b8571cf118aeeeef10ea49292
Mick Thomson <mickt9972@gmail.com>
2024-08-13 18:02:07 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x0AE820206B3fe6AFbEA5448A76BC507Be04f5c71'
+export const ProxyAdmin = '0xa6f4552a0c746EE2cAf32f74513aE3E4D446D807'
+export const SWTRImplementation = '0x3e56274d9c7C2959A34aCd143cA066595BE9beb9'
     
